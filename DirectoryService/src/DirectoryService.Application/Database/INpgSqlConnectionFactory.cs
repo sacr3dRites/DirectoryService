833cdf10776031922f229ea0941ab2b5b35296d9
sacr3dRites <astafiev.simon@gmail.com>
2026-06-08 23:40:52 +0500

    Добавил ConnectionFactory

---
+++ DirectoryService/src/DirectoryService.Application/Database/INpgSqlConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using System.Data;
+
+namespace DirectoryService.Application.Database;
+
+public interface INpgSqlConnectionFactory
+{
+    Task<IDbConnection> CreateConnectionAsync();
+}
---
+++ DirectoryService/src/DirectoryService.Infrastructure/Database/NpgSqlConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using DirectoryService.Application.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace DirectoryService.Infrastructure.Database;
+
+public class NpgSqlConnectionFactory : INpgSqlConnectionFactory, IDisposable, IAsyncDisposable
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public NpgSqlConnectionFactory(IConfiguration configuration)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString("DirectoryServiceDb"));
+        dataSourceBuilder
+            .UseLoggerFactory(CreateLoggerFactory());
+
+        _dataSource = dataSourceBuilder.Build();
+    }
+
+    public async Task<IDbConnection> CreateConnectionAsync()
+    {
+        return await _dataSource.OpenConnectionAsync();
+    }
+
+    private ILoggerFactory CreateLoggerFactory() => LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+    public void Dispose() => _dataSource.Dispose();
+
+    public async ValueTask DisposeAsync() => await _dataSource.DisposeAsync();
+}
