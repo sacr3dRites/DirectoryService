051ce052cfd92c97300f4d2a7b1356fee0831ea9
sacr3dRites <astafiev.simon@gmail.com>
2026-09-05 18:46:02 +0500

    Добавил хэндлер получения корневых департаментов

---
+++ DirectoryService/src/DirectoryService.Application/Departments/GetDepartmentTrees/GetAllRootDepartmentTreesHandler.cs
@@ -0,0 +1,52 @@
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Application.Abstractions;
+using DirectoryService.Application.PaginationUtils;
+using DirectoryService.Contracts.Departments;
+using DirectoryService.Infrastructure.Database;
+using DirectoryService.Shared.CustomErrors;
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Application.Departments.GetDepartmentTrees;
+
+public class
+    GetAllRootDepartmentTreesHandler : IQueryHandler<GetAllRootDepartmentTreesQuery, PagedResult<DepartmentTree>>
+{
+    private readonly IReadDbContext _context;
+
+    public GetAllRootDepartmentTreesHandler(IReadDbContext readDbContext)
+    {
+        _context = readDbContext;
+    }
+
+    public async Task<Result<PagedResult<DepartmentTree>, Errors>> Handle(GetAllRootDepartmentTreesQuery query,
+        CancellationToken cancellationToken)
+    {
+        if (query.Page < 1 || query.PageSize is < 1 or > 100)
+            return GeneralErrors.ValueIsInvalid("параметры пагинации").ToErrors();
+
+        var rootDeps = _context.DepartmentsRead
+            .Where(dep => dep.Parent == null);
+
+        var totalCount = await rootDeps.CountAsync(cancellationToken);
+
+        var trees = await rootDeps
+            .OrderBy(dep => query.SortBy)
+            .ThenBy(dep => dep.Id)
+            .Skip((query.Page - 1) * query.PageSize)
+            .Take(query.PageSize)
+            .Select(dep => new DepartmentTree(
+                dep.Id,
+                dep.Name.Value,
+                dep.Identifier.Value,
+                dep.Path.Value,
+                dep.Depth,
+                dep.Children.Any(child => child.IsActive),
+                dep.Children.Count(child => child.IsActive)
+            ))
+            .ToArrayAsync(cancellationToken);
+
+        var pageCount = (int)Math.Ceiling(totalCount / (double)query.PageSize);
+
+        return new PagedResult<DepartmentTree>(trees, query.Page, query.PageSize, pageCount, totalCount);
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Contracts/Departments/GetAllRootDepartmentTreesQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DirectoryService.Contracts.Shared;
+
+namespace DirectoryService.Contracts.Departments;
+
+public record GetAllRootDepartmentTreesQuery(
+    SortBy SortBy,
+    SortDirection SortDirection,
+    [Range(1, Int32.MaxValue)] int Page = 1,
+    [Range(1, 100)] int PageSize = 20);
