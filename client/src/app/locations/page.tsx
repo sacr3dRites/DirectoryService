aa6d5252463e7ae59370920d8670e1bbd633f0eb
sacr3dRites <astafiev.simon@gmail.com>
2026-09-25 18:55:11 +0500

    added basic routing

---
+++ client/src/app/locations/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Локации | Directory Service",
+};
+
+export default function Locations() {
+  return (
+    <main>
+      <h1>Локации</h1>
+      <section />
+    </main>
+  );
+}
